eb304d7839ae305b0a76e8c491878badcc1d5857
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-29 01:09:08 +0000

    Chat messages now show user's first name

--- chatServer.js
+++ chatServer.js
@@ -1,16 +1,33 @@
+"use strict"
+
+require('dotenv').config();
+
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const PORT = 3002;
 
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+
+//
+// JWT Validation
+//
+
+var jwt = require('jwt-simple');
+var secret = process.env.JWT_SECRET;
+var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MTY2.u5u4hkvjrDZPcxPXZzFP9i5eRuIoSFv3Z5ofnBucBWo'
+var decoded = jwt.decode(token, secret);
+
+// console.log(decoded);
+
+
+
+//
+// Sockets
+//
 
 io.on("connection", function(socket) {
   console.log("a user connected");
-  socket.broadcast.emit('message', { some: 'you connected' });
 
   socket.on("message", function(message) {
     console.log("I received a message", message);
